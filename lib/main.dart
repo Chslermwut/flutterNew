52b4611248f20fc44792513df8df28acfebbcfe5
Chslermwut <chalermwut13@hotmail.com>
2024-12-03 16:54:28 +0700

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,17 @@ void main() {
           backgroundColor: Colors.blue,
           centerTitle: true,
         ),
-        body: const Text("Hello Flutter"),
+        body: const Home(),
       ),
     ),
   );
 }
 
+class Home extends StatelessWidget {
+  const Home({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello Flutter Content");
+  }
+}
