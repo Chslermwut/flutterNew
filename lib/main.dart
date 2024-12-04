71627d771f7968e71b9bc1dbb8593f5dd116b69a
Chslermwut <chalermwut13@hotmail.com>
2024-12-04 16:10:14 +0700

    stateless

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:learning_flutter/screens/home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       title: "First app",
       home: Scaffold(
         appBar: AppBar(
@@ -13,6 +20,6 @@ void main() {
         ),
         body: const Home(),
       ),
-    ),
-  );
+    );
+  }
 }
