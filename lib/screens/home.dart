6fe246ee920df7a62dae756cf18f8fe243f5ea50
Chslermwut <chalermwut13@hotmail.com>
2024-12-04 11:40:49 +0700

    images asset

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,9 +6,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+     crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-      Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'
+      Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+      width: 150,
+      height: 150,
       ),
+      const SizedBox(height: 30,),
+      Image.asset(
+        'assets/images/pic1.png',
+        width: 150,
+        height: 150,
+        ),
+        const SizedBox(height: 30,),
+      Image.asset(
+        'assets/images/pic2.png',
+        width: 150,
+        height: 150,
+        ),
+      //   const SizedBox(height: 30,),
+      // Image.asset(
+      //   'assets/images/nurse.jpeg',
+      //   width: 150,
+      //   height: 150,
+      //   ),
+      //   const SizedBox(height: 30,),
+      // Image.asset(
+      //   'assets/images/police.jpg',
+      //   width: 150,
+      //   height: 150,
+      //   )
     ],
   );
  }
